4f95fb06e04ac25b22932291804b594d55878c4a
shafayat666 <shafayatmahin5@gmail.com>
2024-12-14 15:22:25 +0600

    added flags

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,19 +5,56 @@ import "./Countries.css";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const [visitedFlags, setVisitedFlags] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then(res => res.json())
       .then(data => setCountries(data));
-  }, [])
+  }, []);
+
+  const handleVisitedCountry = country => {
+    console.log("Add this to your visited country");
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  }
+
+  const handleVisitedFlags = flag => {
+    console.log("adding flag");
+    const newVisitedFlags = [...visitedFlags, flag];
+    setVisitedFlags(newVisitedFlags);
+  }
 
   return (
     <div>
       <h3>Countries: {countries.length}</h3>
+      {/* visited countries */}
+      <div>
+        <h5>Visited Countries: {visitedCountries.length}</h5>
+        <ul>
+          {
+            visitedCountries.map(country => 
+            <li key={country.cca3}>
+              {country.name.common}
+            </li>)
+          }
+        </ul>
+      </div>
+      <div className="flag-container">
+          {
+            visitedFlags.map((flag, index) => <img key={index} src={flag}></img>)
+          }
+      </div>
+      {/* display all countries */}
       <div className="country-container">
         {
-          countries.map(country => <Country key={country.cca3} country={country} />)
+          countries.map(country => <Country 
+          key={country.cca3} 
+          country={country}
+          handleVisitedCountry={handleVisitedCountry}
+          handleVisitedFlags = {handleVisitedFlags} />)
+
         }
       </div>
     </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import "./Country.css";
 
-const Country = ({country}) => {
-    const {name, flags, population, area} = country;
+const Country = ({ country, handleVisitedCountry, handleVisitedFlags }) => {
+    const { name, flags, population, area, cca3 } = country;
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
 
     return (
-        <div className="country">
-            <h3>Name: {name.common}</h3>
+        <div className={`country ${visited && 'visited'}`}>
+            <h3 style={{ color: visited ? 'Purple' : 'white' }}>Name: {name.common}</h3>
             <img src={flags.png} />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
+
+            <button onClick={() => handleVisitedCountry(country)}>Mark visited</button>
+            <br />
+            <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Flag</button>
+            <button onClick={handleVisited}>{visited ? "Visited" : "Going"}</button>
+            {visited ? "I have visited this country" : "I want to visit"}
         </div>
     );
 };
