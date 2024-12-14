c6ed7062e22304bf2fb626857af4343ea52c2c8a
shafayat666 <shafayatmahin5@gmail.com>
2024-12-14 11:34:04 +0600

    added country names and flags

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,19 +1,23 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Country from "./Country/Country";
 
 const Countries = () => {
-    const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
 
-    useEffect(() => {
-        fetch("https://restcountries.com/v3.1/all")
-        .then(res => res.json())
-        .then(data => console.log(data));
-    }, [])
-    return (
-        <div>
-            <h3>Countries</h3>
-        </div>
-    );
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then(res => res.json())
+      .then(data => setCountries(data));
+  }, [])
+  return (
+    <div>
+      <h3>Countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country key={country.cca3} country={country}/>)
+      }
+    </div>
+  );
 };
 
 export default Countries;
